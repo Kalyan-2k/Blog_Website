5772a7f1fc7093ef61c8467a87bcf66639272584
Kalyan-2k <97961782+Kalyan-2k@users.noreply.github.com>
2022-12-31 14:28:21 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint,render_template,request,flash,redirect,url_for,session
-from .models import Post,User,Comment,Like
+from models import Post,User,Comment,Like
 from datetime import datetime
 import codecs
 
