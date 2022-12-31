3eb6d969e1902003c52db2c3175480060bf51a96
prince <nprince061@gmail.com>
2022-12-31 13:51:43 +0530

    changed main

--- Routes/main.py
+++ Routes/main.py
@@ -1,6 +1,6 @@
 from Routes import create_app
+app=create_app()
 
-if __name__=="__main__":
-    app=create_app()
+if __name__=="__main__":    
     app.run(debug=True,host='0.0.0.0')
     # app.run(host='0.0.0.0')   # experiment
