c1c293d34b98efca0f4481d3dd100ddb1d5ed8c3
Kalyan-2k <97961782+Kalyan-2k@users.noreply.github.com>
2022-12-31 12:04:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from Routes import create_app
 
 if __name__=="__main__":
     app=create_app()
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
     # app.run(host='0.0.0.0')   # experiment
